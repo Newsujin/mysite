c86a3e20a5a9166e70b0751227ce8360bb66f51d
sujin Park <chemd78@naver.com>
2024-10-04 20:29:52 +0900

    :sparkles: feat: get_object_or_404 함수 사용

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
@@ -30,10 +30,14 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("Question does not exist")
+    # 1
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except:
+    #     raise Http404("Question does not exist")
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
