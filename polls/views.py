4ed38f01d42f655690e01cbb93e9bb85daf3e1e6
sujin Park <chemd78@naver.com>
2024-10-06 23:32:46 +0900

    :sparkles: feat: vote() 뷰에서 선택 항목 득표수 증가, 성공 시 결과 페이지 리디렉션

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.http import Http404
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     # 1
@@ -29,6 +30,7 @@ def index(request):
     }
     return render(request, "polls/index.html", context)
 
+
 def detail(request, question_id):
     # 1
     # try:
@@ -47,4 +49,27 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # 1
+    # return HttpResponse("You're voting on question %s." % question_id)
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        seleceted_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        seleceted_choice.votes = F("votes") + 1
+        seleceted_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
