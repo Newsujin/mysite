f76d75ca9401845a6b9fc96dfa36b9bed789d909
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-10-07 22:54:34 +0900

    [Part4] Form, Generic view (#2)
    * :sparkles: feat: 질문 선택 항목에 대한 라디오 버튼 표시
    
    * :sparkles: feat: vote() 뷰에서 선택 항목 득표수 증가, 성공 시 결과 페이지 리디렉션
    
    * :sparkles: feat: 투표 결과 페이지 템플릿 및 뷰 추가
    
    * :sparkles: feat: URLconf 수정
    
    제네릭 뷰 사용 및 URL 매개변수 question_id에서 pk로 변경
    
    * :hammer: refactor: 제네릭 뷰로 index, detail, results 뷰 대체

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.http import Http404
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
-def index(request):
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+# def index(request):
     # 1
     # return HttpResponse("Hello, World. You're at the polls index.")
 
@@ -23,13 +35,19 @@ def index(request):
     # return HttpResponse(template.render(context, request))
     
     # 4
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
-    return render(request, "polls/index.html", context)
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # context = {
+    #     "latest_question_list": latest_question_list
+    # }
+    # return render(request, "polls/index.html", context)
+    
 
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+# def detail(request, question_id):
     # 1
     # try:
     #     question = Question.objects.get(pk=question_id)
@@ -37,14 +55,47 @@ def detail(request, question_id):
     #     raise Http404("Question does not exist")
 
     # 2
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, "polls/detail.html", {"question": question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+# def results(request, question_id):
+    # 1
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+
+    # 2
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # 1
+    # return HttpResponse("You're voting on question %s." % question_id)
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        seleceted_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        seleceted_choice.votes = F("votes") + 1
+        seleceted_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
